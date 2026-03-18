cd09d01ae1455c96d3a700f5e20dbbe12e32841b
Ömer Berkin Kıstı <berkin979797@gmail.com>
2026-03-18 16:46:51 +0300

    database connection

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,7 +1,7 @@
 // Unless explicitly defined, set NODE_ENV as development:
 process.env.NODE_ENV ??= 'development';
 
-import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
+import { ApplicationCommandRegistries, RegisterBehavior, container } from '@sapphire/framework';
 import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-editable-commands/register';
 import '@sapphire/plugin-logger/register';
@@ -11,6 +11,7 @@ import * as colorette from 'colorette';
 import { join } from 'path';
 import { inspect } from 'util';
 import { srcDir } from './constants';
+import { PrismaClient } from "@prisma/client";
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
@@ -24,8 +25,18 @@ inspect.defaultOptions.depth = 1;
 // Enable colorette
 colorette.createColors({ useColor: true });
 
+container.db = new PrismaClient();
+
 declare module '@skyra/env-utilities' {
 	interface Env {
+	DATABASE_URL: string;
 		OWNERS: ArrayString;
 	}
 }
+
+declare module '@sapphire/pieces' {
+	interface Container {
+		db: PrismaClient;
+	}
+}
+
